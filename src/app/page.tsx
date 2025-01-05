5262975eda48a43c62685c9117f953405f43c2c8
amirrajj-dev <amiramraja@gmail.com>
2025-01-05 22:42:44 +0330

    fixed hydration error next js and updated layout structure👽💚

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl p-20">
-        <div className="grid lg:grid-cols-12">
-          <div className="lg:col-span-3">ewq</div>
-          <div className="lg:col-span-9">eqw</div>
+      <div className="max-w-7xl py-8 mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+          <div className="col-span-1 lg:col-span-3 bg-white dark:bg-neutral-900 p-4 rounded-lg shadow-md">
+            <p>Content for sidebar</p>
+          </div>
+          <div className="col-span-1 lg:col-span-9 bg-white dark:bg-neutral-900 p-4 rounded-lg shadow-md">
+            <p>Main content area</p>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
