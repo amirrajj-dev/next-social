efc640eacc335f7b74d86428670eae5b63f0d6d8
amirrajj-dev <amiramraja@gmail.com>
2025-01-12 15:58:21 +0330

    changes get notif action functionallity for better ux

--- src/actions/notification.action.tsx
+++ src/actions/notification.action.tsx
@@ -6,35 +6,41 @@ import { usersModel } from "@/utils/models/user.model"
 import { IUser } from "@/types/types"
 import { notificationModel } from "@/utils/models/notification.model"
 
-export const getNotificationsAction = async ()=>{
+export const getNotificationsAction = async () => {
     try {
-        await connectToDb()
-        const currentUser : IUser = (await getCurrentUserAction()).data
-        const user : IUser = await usersModel.findById(currentUser._id) as IUser
+        await connectToDb();
+        const currentUser: IUser = (await getCurrentUserAction()).data;
+        const user: IUser = await usersModel.findById(currentUser._id) as IUser;
 
         const userNotifications = await notificationModel.find({ receiver: user._id }).populate([
             {
                 path: 'sender',
-                select : 'img fullname',
-                
+                select: 'img fullname',
             },
             {
-                path : 'post',
-                select : 'content image',
+                path: 'post',
+                select: 'content image',
             },
             {
-                path : 'comment',
-                select : 'content img post',
-                populate : {
-                    path : 'post',
-                    select : 'content image',
+                path: 'comment',
+                select: 'content img post',
+                populate: {
+                    path: 'post',
+                    select: 'content image',
                 }
             }
-        ]).sort({_id : -1})
+        ]).sort({ _id: -1 });
 
-      
-        return {message : 'notifications fetched successfully' , success : true , data : userNotifications}
+        // Mark all notifications as read and then delete them
+        await notificationModel.updateMany({ receiver: user._id }, { $set: { read: true } });
+        await notificationModel.deleteMany({ receiver: user._id });
+        //deleting all user notifications form user notifications field
+        await usersModel.updateOne({ _id: user._id }, { $set: {
+            notifications: []
+        }})
+
+        return { message: 'notifications fetched, marked as read, and deleted successfully', success: true, data: userNotifications };
     } catch (error) {
-        return {message : 'error fetching notifications' , success : false , error}
+        return { message: 'error fetching notifications', success: false, error };
     }
-} 
+}
